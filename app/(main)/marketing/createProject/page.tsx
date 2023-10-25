fa72fba77b21609be892919c00e527b8c86b5519
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-10-25 11:12:26 +0700

    Add component input

--- app/(main)/marketing/createProject/page.tsx
+++ app/(main)/marketing/createProject/page.tsx
@@ -1,7 +1,9 @@
+import CreateProjectBaseData from "@/components/forms/CreateProjectBaseData";
+
 export default function Home() {
     return (
-        <div className=" w-screen h-full ">
-
+        <div className=" w-fit flex ">
+            <CreateProjectBaseData></CreateProjectBaseData>
         </div>
     )
 }
