0daafff9296152d5740d865442512fd149978283
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-10-27 15:56:57 +0700

    Combined from albert to develop and fix the libraries issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+import React, {useState} from "react";
+import ClientForm from "@/components/forms/ClientForm";
+import IdCheckForm from "@/components/forms/IdCheckForm";
+
 export default function Home() {
+  const [resiNumber, setResiNumber] = useState('')
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl font-bold">Client Page</h1>
-    </main>
+    <>
+      <div className="w-screen flex flex-col md:flex-row">
+        <IdCheckForm setResiNumber={setResiNumber} />
+        {resiNumber !== '' ? <ClientForm resiNumber={resiNumber} /> : <div className="w-full md:w-2/3 h-[90vh] flex flex-col m-6 mx-10 bg-ghost_white rounded-xl"/>}
+      </div>
+    </>
   );
 }
