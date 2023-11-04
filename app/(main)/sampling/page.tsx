5f833dc0c574b19bc4342f00374e768d24dc0930
Dimas <dimas.dermawan@ui.ac.id>
2023-11-04 17:54:56 +0700

    Add Sampling Project Page

--- app/(main)/sampling/page.tsx
+++ app/(main)/sampling/page.tsx
@@ -1,4 +1,8 @@
+import { redirect } from "next/navigation"
+
 export default function Sampling() {
+    redirect("/sampling/project")
+    
     return (
         <div className="w-screen h-screen bg-violet-200">
             
