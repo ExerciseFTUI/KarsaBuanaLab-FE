61047d26c669784ec0551e6a73db776606daffa9
Dimas <dimas.dermawan@ui.ac.id>
2023-11-09 07:35:43 +0700

    Adding dummy data for project & adding sample type

--- lib/type.ts
+++ lib/type.ts
@@ -1,23 +1,39 @@
 export type ProjectType = {
-  id: string;
-  noPenawaran: string;
-  judul: string;
-  namaCustomer: string;
-  lokasi: string;
-  cp: string;
-  nilaiPenawaran: number;
-  createdAt: string;
-};
+  id: string
+  noPenawaran: string
+  judul: string
+  namaCustomer: string
+  lokasi: string
+  cp: string
+  nilaiPenawaran: number
+  createdAt: string
+}
+
+export type SampleType = {
+  sample_name: string
+  harga: string
+  fileId: string
+  regulation: string
+  location: string
+}
 
 export type ProjectSamplingType = {
-  id: string;
-  noPenawaran: string;
-  judul: string;
-  namaCustomer: string;
-  lokasi: string;
-  cp: string;
-  nilaiPenawaran: number;
-  createdAt: string;
-  status: string;
-};
+  no_penawaran: string
+  no_sampling: string
+  client_name: string
+  project_name: string
+  alamat_kantor: string
+  alamat_sampling: string
+  surel: string
+  contact_person: string
+  status: string
+  folder_id: string
+  password: string
+  jumlah_revisi: number
+  valuasi_proyek: number
+  surat_penawaran: string
+  created_year: string
+  sampling_list: SampleType[]
 
+  file: { file_nama: string; file_id: string }
+}
