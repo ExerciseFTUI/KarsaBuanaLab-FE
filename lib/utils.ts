00e458634e2506bea24129358e776e91bce6a8b1
Dimas <dimas.dermawan@ui.ac.id>
2023-11-09 18:02:42 +0700

    Adding converter number to rupiah function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -13,4 +13,11 @@ export async function fetcher(url: string) {
     throw new Error(data)
   }
   return JSON.parse(data)
+}
+
+export function rupiah(number: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(number)
 }
