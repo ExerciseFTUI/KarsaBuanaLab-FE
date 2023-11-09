d082fe3fba8752ac145f9256e9f42104f939978f
Dimas <dimas.dermawan@ui.ac.id>
2023-11-09 15:03:59 +0700

    Changing flex styles

--- app/(main)/sampling/project/[np]/page.tsx
+++ app/(main)/sampling/project/[np]/page.tsx
@@ -1,10 +1,10 @@
-import Project from "@/components/sampling/projectDetails"
+import Project from "@/components/sampling/projectDetail"
 
 // prettier-ignore
 export default function SamplingProject({ params }: { params: { np: string } }) { 
   return (
-    <div className="flex flex-col w-full border-t-light_brown border-t-2 py-4 lg:flex-row">
-      <Project params={params} />
+    <div className="border-t-light_brown border-t-2 py-4">
+      <Project params={params} className="flex-wrap" />
     </div>
   )
 }
