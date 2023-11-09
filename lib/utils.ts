ac07e9b61dfcceee97c254c17f2c2c851971da8c
Dimas <113928758+den-dimas@users.noreply.github.com>
2023-11-09 04:58:33 +0000

    Adding fetcher function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function fetcher(url: string) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  if (res.status !== 200) {
+    throw new Error(data)
+  }
+  return JSON.parse(data)
+}
