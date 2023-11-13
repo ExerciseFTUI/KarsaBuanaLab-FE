7b7a61f6dc99cdf28788e7430e855282b2aee94b
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-11-13 16:27:09 +0700

    Fixed again

--- app/api/sampling/assignment-letter/route.ts
+++ app/api/sampling/assignment-letter/route.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 import { assignmentLetterData } from "@/constants/samplingData"
 
-export async function GET(_req: NextApiRequest) {
+export async function GET(_req: NextRequest | Request) {
   return NextResponse.json(JSON.stringify(assignmentLetterData))
 }
