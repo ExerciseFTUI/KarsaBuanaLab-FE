71279779510ac77b3d90370a074d8eda0bce635f
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-11-13 16:22:38 +0700

    Fixed problem on vercel

--- app/api/sampling/assignment-letter/[np]/route.ts
+++ app/api/sampling/assignment-letter/[np]/route.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 import { assignmentLetterData } from "@/constants/samplingData"
 
 export async function GET(
-  _request: NextApiRequest,
+  _request: NextRequest | Request,
   { params }: { params: { np: string } }
 ) {
   const data = assignmentLetterData.find((p) => p.no_penawaran == params.np)
