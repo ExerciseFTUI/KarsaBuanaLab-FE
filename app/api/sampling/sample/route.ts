116f61d86faf449fd0669d640e10dc07aa390728
Dimas <dimas.dermawan@ui.ac.id>
2023-12-25 22:25:02 +0700

    Adjusting API Route Mock

--- app/api/sampling/sample/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { NextRequest, NextResponse } from "next/server"
-
-import { sampleSamplingData } from "@/constants/samplingData"
-
-export async function GET(_req: NextRequest | Request) {
-  return NextResponse.json(JSON.stringify(sampleSamplingData))
-}
