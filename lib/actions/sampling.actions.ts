f2988e79783d4601d9d81a906465550576e0ebad
Dimas <dimas.dermawan@ui.ac.id>
2023-12-25 22:24:28 +0700

    Updating Sampling Controller

--- lib/actions/sampling.actions.ts
+++ lib/actions/sampling.actions.ts
@@ -3,7 +3,7 @@ import {BaseApiResponse} from '../models/baseApiResponse.model'
 import { Sampling } from '../models/sampling.model';
 import { Project } from '../models/project.model';
 
-const apiBaseUrl = process.env.API_BASE_URL || '';
+const apiBaseUrl = process.env.API_BASE_URL || 'http://localhost:3000/api';
 
 export const getSampleById = async (year: string, sampleId : string) : Promise<BaseApiResponse<Sampling>> => {
   try {
@@ -19,7 +19,35 @@ export const getSampleById = async (year: string, sampleId : string) : Promise<B
 
 }
 
-export const getSampleByAccount = async (year: string, accountId : string) : Promise<BaseApiResponse<[Project]>> => {
+export const getSampleByYear = async (year: string, page: string) : Promise<BaseApiResponse<Sampling[]>> => {
+  try {
+
+    const response = await axios.get(`${apiBaseUrl}/sampling/get/${year}`);
+
+    const result: Sampling[] = response.data.result.filter((s: Sampling) => {
+      if (page == "project")
+        return s.status == "FINISHED" || s.status == "NOT ASSIGNED"
+      else if (page == "assignment-letter")
+        return s.status == "ASSIGNED"
+      else
+        return s.status == "Get Sample" || s.status == "Verifying" || s.status == "Revision"
+    })
+
+    const data: BaseApiResponse<Sampling[]> = {
+      message: "Success",
+      result
+    }
+
+    return data;
+
+  } catch (error: any) {
+    console.error('Error getting sample list', error.message);
+    throw new Error('Failed to get sample list');
+  }
+
+}
+
+export const getSampleByAccount = async (year: string, accountId : string) : Promise<BaseApiResponse<[Sampling]>> => {
   try {
 
     const response = await axios.get(`${apiBaseUrl}/sampling/sample/${year}`, {
@@ -28,7 +56,7 @@ export const getSampleByAccount = async (year: string, accountId : string) : Pro
       },
     });
 
-    return response.data as BaseApiResponse<[Project]>;
+    return response.data as BaseApiResponse<[Sampling]>;
 
   } catch (error: any) {
     console.error('Error getting sample', error.message);
