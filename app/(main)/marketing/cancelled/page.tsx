37fbe6eede32df37e6f3df28f8c4de6739b1da7d
RadityaDito <raditya.ihsan@ui.ac.id>
2024-01-03 19:03:31 +0700

    Fix build error by checking the project result

--- app/(main)/marketing/cancelled/page.tsx
+++ app/(main)/marketing/cancelled/page.tsx
@@ -1,9 +1,14 @@
 // import CancelledTable from "@/components/marketing/Tables/CancelledTable";
 import { DataTable } from "@/components/DataTable";
 import { getbyStatus } from "@/lib/actions/marketing.actions";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const projects = await getbyStatus("cancelled");
+
+  if (!projects.result) {
+    redirect("/marketing");
+  }
 
   return (
     <div className="flex justify-between w-full h-screen">
--- app/(main)/marketing/running/page.tsx
+++ app/(main)/marketing/running/page.tsx
@@ -3,9 +3,14 @@ import { columns } from "@/components/columns";
 
 import { getbyStatus } from "@/lib/actions/marketing.actions";
 import { Project } from "@/lib/models/project.model";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const projects = await getbyStatus("running");
+
+  if (!projects.result) {
+    redirect("/marketing");
+  }
 
   return (
     <div className="flex justify-between w-full h-screen">
