e0e4d80da01c64eb21c8c52f03cdbdba7f779994
Dimas <dimas.dermawan@ui.ac.id>
2024-01-11 17:15:25 +0700

    Removing unnecessary dummy data

--- constants/receiveData.ts
+++ constants/receiveData.ts
@@ -1,25 +1,25 @@
-import { ReceiveSamplingType, SampleType } from "@/lib/type";
-import { randomInt, randomUUID } from "crypto";
-import projectJson from "@/constants/projectData.json";
+// import { ReceiveSamplingType, SampleType } from "@/lib/type";
+// import { randomInt, randomUUID } from "crypto";
+// import projectJson from "@/constants/projectData.json";
 
-function generateRandomData(length: number = 100): ReceiveSamplingType[] {
-  let data: ReceiveSamplingType[] = [];
+// function generateRandomData(length: number = 100): ReceiveSamplingType[] {
+//   let data: ReceiveSamplingType[] = [];
 
-  for (let i = 1; i <= length; i++) {
-    let temp: ReceiveSamplingType = {
-      no_penawaran: "PNW" + (i < 10 ? "0" + i : i + ""),
-      project_name: "Project " + i,
-      alamat_kantor: "Kantor " + i,
-      alamat_sampling: "Alamat Sample " + i,
-      contact_person: "Orang " + i,
-    };
+//   for (let i = 1; i <= length; i++) {
+//     let temp: ReceiveSamplingType = {
+//       no_penawaran: "PNW" + (i < 10 ? "0" + i : i + ""),
+//       project_name: "Project " + i,
+//       alamat_kantor: "Kantor " + i,
+//       alamat_sampling: "Alamat Sample " + i,
+//       contact_person: "Orang " + i,
+//     };
 
-    data.push(temp);
-  }
+//     data.push(temp);
+//   }
 
-  return data;
-}
+//   return data;
+// }
 
-const projectData: ReceiveSamplingType[] = JSON.parse(projectJson);
+// const projectData: ReceiveSamplingType[] = JSON.parse(projectJson);
 
-export { projectData };
+// export { projectData };
