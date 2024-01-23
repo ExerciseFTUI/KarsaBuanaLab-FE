bbae704e8283f880464bb515f5ee89f13c5b279c
Dimas <dimas.dermawan@ui.ac.id>
2024-01-24 01:49:30 +0700

    FIX error href cant be passed empty strings ("")

--- constants/receiveData.ts
+++ constants/receiveData.ts
@@ -5,15 +5,15 @@ import projectJson from "@/constants/data/projectData.json";
 // function generateRandomData(length: number = 100): ReceiveSamplingType[] {
 //   let data: ReceiveSamplingType[] = [];
 
-  for (let i = 1; i <= length; i++) {
-    let temp: ReceiveSamplingType = {
-      _id: "1",
-      no_penawaran: "PNW" + (i < 10 ? "0" + i : i + ""),
-      project_name: "Project " + i,
-      alamat_kantor: "Kantor " + i,
-      alamat_sampling: "Alamat Sample " + i,
-      contact_person: "Orang " + i,
-    };
+  // for (let i = 1; i <= length; i++) {
+  //   let temp: ReceiveSamplingType = {
+  //     _id: "1",
+  //     no_penawaran: "PNW" + (i < 10 ? "0" + i : i + ""),
+  //     project_name: "Project " + i,
+  //     alamat_kantor: "Kantor " + i,
+  //     alamat_sampling: "Alamat Sample " + i,
+  //     contact_person: "Orang " + i,
+  //   };
 
 //     data.push(temp);
 //   }
