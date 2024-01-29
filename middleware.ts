1376d9867e30c205ece2a57d62e861ea71f62a60
RadityaDito <raditya.ihsan@ui.ac.id>
2024-01-30 04:14:53 +0700

    Add role checker middleware

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,42 @@
 // Without a defined matcher, this one line applies next-auth
 // to the entire project
-import { withAuth } from "next-auth/middleware";
+import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  pages: {
-    signIn: "/login", //Sign in page
-  },
-});
-
-// export default withAuth(
-//   // `withAuth` augments your `Request` with the user's token.
-//   function middleware(request: NextRequestWithAuth) {
-//       // console.log(request.nextUrl.pathname)
-//       // console.log(request.nextauth.token)
-
-//       if (request.nextUrl.pathname.startsWith("/extra")
-//           && request.nextauth.token?.role !== "admin") {
-//           return NextResponse.rewrite(
-//               new URL("/denied", request.url)
-//           )
-//       }
-
-//       if (request.nextUrl.pathname.startsWith("/client")
-//           && request.nextauth.token?.role !== "admin"
-//           && request.nextauth.token?.role !== "manager") {
-//           return NextResponse.rewrite(
-//               new URL("/denied", request.url)
-//           )
-//       }
+// export default withAuth({
+//   pages: {
+//     signIn: "/login", //Sign in page
 //   },
-//   {
-//       callbacks: {
-//           authorized: ({ token }) => !!token
-//       },
-//   }
-// )
+// });
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(request: NextRequestWithAuth) {
+    // console.log(request.nextUrl.pathname);
+    // console.log(request.nextauth.token);
+    // if (
+    //   request.nextUrl.pathname.startsWith("/marketing") &&
+    //   request.nextauth.token?.division.toLowerCase() !== "marketing"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/denied", request.url));
+    // }
+    // if (
+    //   request.nextUrl.pathname.startsWith("/sampling") &&
+    //   request.nextauth.token?.division.toLowerCase() !== "sampling"
+    //   //&& request.nextauth.token?.role !== "manager"
+    // ) {
+    //   return NextResponse.rewrite(new URL("/denied", request.url));
+    // }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages: {
+      signIn: "/login", //Sign in page
+    },
+  }
+);
 
 // Applies next-auth only to matching routes - can be regex
 // Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
