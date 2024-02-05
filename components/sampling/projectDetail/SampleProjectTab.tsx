f834a3090833051ecac6036730a4530524d8c30c
Dimas <dimas.dermawan@ui.ac.id>
2024-02-06 00:31:37 +0700

    Update jadwal and user assignment

--- components/sampling/projectDetail/SampleProjectTab.tsx
+++ components/sampling/projectDetail/SampleProjectTab.tsx
@@ -30,20 +30,49 @@ import { Project } from "@/lib/models/project.model"
 import { DateRange } from "react-day-picker"
 import { useRouter } from "next/navigation"
 import LoadingScreen from "@/components/LoadingScreen"
+import { SamplingRequestData } from "@/lib/type"
 
 interface Params {
-  data: Project
+  data: SamplingRequestData
 }
 
 export default function SampleProjectTab({ data }: Params) {
+  const { files, user, project } = data
+
+  const { jadwal_sampling } = project
+
+  const from = (
+    jadwal_sampling
+      ? jadwal_sampling.from
+        ? new Date(jadwal_sampling.from)
+        : null
+      : null
+  ) as Date
+  const to = (
+    jadwal_sampling
+      ? jadwal_sampling.to
+        ? new Date(jadwal_sampling.to)
+        : null
+      : null
+  ) as Date
+
+  let initialState: RowSelectionState = {}
+
+  if (project.project_assigned_to.length)
+    user.forEach((u: User, i: number) => {
+      if (project.project_assigned_to.includes(u._id) == true)
+        initialState[`${i}`] = true
+    })
+
   const [date, setDate] = React.useState<DateRange | undefined>({
-    from: undefined,
-    to: undefined,
+    from: from,
+    to: to,
   })
-  const [rowSelection, setRowSelection] = React.useState<RowSelectionState>({})
+  const [rowSelection, setRowSelection] =
+    React.useState<RowSelectionState>(initialState)
 
   const table = useReactTable({
-    data: data.project_assigned_to,
+    data: !!user ? user : [],
     columns: groupUserSelectableColumns,
     getCoreRowModel: getCoreRowModel(),
     onRowSelectionChange: setRowSelection,
@@ -56,26 +85,26 @@ export default function SampleProjectTab({ data }: Params) {
   const [isLoading, setIsLoading] = useState(false)
 
   // prettier-ignore
-  async function addAssigned(e: any) {
+  async function addProjectDeadline(e: any) {
     const assigned = table
-      .getFilteredSelectedRowModel()
-      .rows.map((r) => data.project_assigned_to[r.index]._id)
+      .getSelectedRowModel()
+      .rows.map((r) => user[r.index]._id)
 
-    if (assigned.length == 0 || date == null) return
-    
+    if (assigned.length == 0 || !date || !date.from ) return
+
     setIsLoading(true)
 
-    const response = await assignProject(data._id, assigned, date?.toString() as string)
+    const response = await assignProject(data.project._id, assigned, {from: date?.from.toString() || null, to: date?.to?.toString() || null})
+
+    setIsLoading(false)
 
     if (!response) {
       alert("Failed to assign project!")
-      return
     } else {
       alert("Success")
-      router.push("/sampling/project/" + data._id)
     }
-
-    setIsLoading(false)
+    
+    router.refresh()
   }
 
   return (
@@ -86,20 +115,16 @@ export default function SampleProjectTab({ data }: Params) {
 
       <TabsContent className="py-4 w-full" value="dokumen">
         <div className="px-4 py-2 flex flex-col flex-1">
-          <DocumentList data={data} />
+          <DocumentList data={files} />
 
           <div className="flex flex-wrap flex-col max-w-xl">
-            <h1 className="text-xl font-semibold my-5">Assignment Letter</h1>
+            <h1 className="text-xl font-semibold my-5">Surat Tugas</h1>
 
-            <HyperLinkButton title="Assignment Letter" href="/" />
+            <HyperLinkButton
+              title="Surat Tugas"
+              href={files.file.find((f: any) => f.name == "Surat Tugas").url}
+            />
           </div>
-
-          <Button
-            className="w-48 py-4 self-center mt-4 bg-light_brown hover:bg-dark_brown disabled:bg-transparent disabled:text-dark_brown disabled:font-bold disabled:border-2 disabled:border-dark_brown"
-            onClick={(e) => e}
-          >
-            Save
-          </Button>
         </div>
       </TabsContent>
 
@@ -153,7 +178,7 @@ export default function SampleProjectTab({ data }: Params) {
 
           <Button
             className="w-48 py-4 self-center mt-4 bg-light_brown hover:bg-dark_brown disabled:bg-transparent disabled:text-dark_brown disabled:font-bold disabled:border-2 disabled:border-dark_brown"
-            onClick={(e) => addAssigned(e)}
+            onClick={(e) => addProjectDeadline(e)}
           >
             {date == null ? "Choose Deadline" : "Save"}
           </Button>
