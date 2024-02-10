a4b6c441a12136215568af41270db74aea05fb69
Dimas <dimas.dermawan@ui.ac.id>
2024-02-10 22:28:18 +0700

    redirect for admin role

--- app/(main)/sampling/page.tsx
+++ app/(main)/sampling/page.tsx
@@ -4,7 +4,8 @@ import { redirect } from "next/navigation"
 export default async function Sampling() {
   const session = await getSessionServer()
 
-  if (session?.user.role == "SPV") redirect("/sampling/project")
+  if (session?.user.role == "SPV" || session?.user.role == "ADMIN")
+    redirect("/sampling/project")
 
   if (session?.user.role == "USER") redirect("/sampling/sample")
 }
