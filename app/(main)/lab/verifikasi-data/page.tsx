2d41ced66b5b3b9567dd2f75bb9635f493c08692
Dimas <dimas.dermawan@ui.ac.id>
2024-04-02 22:14:18 +0700

    FIX response handling error

--- app/(main)/lab/verifikasi-data/page.tsx
+++ app/(main)/lab/verifikasi-data/page.tsx
@@ -5,9 +5,10 @@ import { getProjectByDivision } from "@/lib/actions/sampling.actions"
 export default async function Home() {
   const res = await getProjectByDivision("Lab")
 
-  const data = res ? (res as any).filter((item: any) => item.lab_status === "NEED ANALYZE") : []
-  // console.log("verifikasi-data", data);
-  
+  const data = !!res
+    ? (res as any).projects.filter((p: any) => p.lab_status === "NEED ANALYZE")
+    : []
+
   return (
     <div className="flex justify-between w-full h-screen">
       <LabDataTable data={data} link="verifikasi-data/" />
