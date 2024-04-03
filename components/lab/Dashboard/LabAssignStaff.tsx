269b014ca4a9791eafc04cfed3750ef3b35108b2
Dimas <dimas.dermawan@ui.ac.id>
2024-04-04 03:00:37 +0700

    Add change project status

--- components/lab/Dashboard/LabAssignStaff.tsx
+++ components/lab/Dashboard/LabAssignStaff.tsx
@@ -8,6 +8,8 @@ import { LabAssignSelect } from "./LabAssignSelect"
 import { useRouter } from "next/navigation"
 import { LabAssignEdit } from "./LabAssignEdit"
 import { format } from "date-fns"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 
 interface params {
   data: SamplingRequestData
@@ -24,47 +26,41 @@ export default function LabAssignStaff({ data, projects }: params) {
     (s) => s.lab_assigned_to.length !== 0
   )
 
+  function saveProject() {}
+
   return (
     <div className="w-full border-0 border-black">
       <h1 className="text-3xl font-bold text-dark_brown text-center">
         Assign Staff
       </h1>
 
-      {
-        /* samplingNotAssigned.length > 0 */ true && (
-          <div className="mt-6">
-            <h1 className="italic">
-              Not Assigned: {samplingNotAssigned.length}
-            </h1>
+      {samplingNotAssigned.length > 0 && (
+        <div className="mt-6">
+          <h1 className="italic">Not Assigned: {samplingNotAssigned.length}</h1>
 
-            <div className="flex flex-col gap-2 mt-6">
-              <div className="grid grid-cols-3 grid-flow-row text-center">
-                <h1 className="font-bold text-lg">Name</h1>
-                <h1></h1>
-                <h1 className="font-bold text-lg">Assign</h1>
-              </div>
-              {samplingNotAssigned.map((s, i) => (
-                <div
-                  key={i}
-                  className="grid grid-cols-3 grid-flow-row gap-2 text-center"
-                >
-                  <h1 className="">{s.sample_name}</h1>
-
-                  <h1></h1>
-
-                  <LabAssignSelect
-                    project={project}
-                    users={user}
-                    sampling={s}
-                  />
-                </div>
-              ))}
+          <div className="flex flex-col gap-2 mt-6">
+            <div className="grid grid-cols-3 grid-flow-row text-center">
+              <h1 className="font-bold text-lg">Name</h1>
+              <h1></h1>
+              <h1 className="font-bold text-lg">Assign</h1>
             </div>
-          </div>
-        )
-      }
+            {samplingNotAssigned.map((s, i) => (
+              <div
+                key={i}
+                className="grid grid-cols-3 grid-flow-row gap-2 text-center"
+              >
+                <h1 className="">{s.sample_name}</h1>
 
-      <div className="mt-6">
+                <h1></h1>
+
+                <LabAssignSelect project={project} users={user} sampling={s} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+
+      <div className="my-6">
         <h1 className="italic">Assigned: {samplingAssigned.length}</h1>
 
         <div className="flex flex-col gap-2 mt-6">
@@ -128,6 +124,25 @@ export default function LabAssignStaff({ data, projects }: params) {
           })}
         </div>
       </div>
+
+      {samplingNotAssigned.length == 0 && (
+        <>
+          <Separator orientation="horizontal" className="bg-dark_brown" />
+
+          <div className="mt-6 w-full flex items-center justify-around">
+            <h1 className="text-xl">
+              Continue this project to <b>Verifikasi Data</b>?
+            </h1>
+
+            <Button
+              className="bg-light_brown hover:bg-dark_brown font-medium text-lg pb-3"
+              onClick={saveProject}
+            >
+              Save
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   )
 }
