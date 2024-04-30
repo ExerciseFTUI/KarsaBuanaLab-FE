ac3e4c13e691d22756c24b6cfa76194e18ba55cd
Dimas <dimas.dermawan@ui.ac.id>
2024-04-30 19:15:05 +0700

    Revisi

--- app/(main)/lab/verifikasi-data/page.tsx
+++ app/(main)/lab/verifikasi-data/page.tsx
@@ -1,17 +1,18 @@
-import LabDataTable from "@/components/lab/LabDataTable"
-import { getProject } from "@/lib/actions/pplhp.actions"
-import { getProjectByDivision } from "@/lib/actions/sampling.actions"
+import LabDataTable from "@/components/lab/LabDataTable";
+import { getLabProjects } from "@/lib/actions/lab.actions";
+import { getProject } from "@/lib/actions/pplhp.actions";
+import { getProjectByDivision } from "@/lib/actions/sampling.actions";
 
 export default async function Home() {
-  const res = await getProjectByDivision("Lab")
+  const res = await getLabProjects();
 
   const data = !!res
-    ? (res as any).projects.filter((p: any) => p.lab_status ===  "IN REVIEW BY SPV")
-    : []
+    ? res.result.filter((p: any) => p.lab_status === "NEED ANALYZE")
+    : [];
 
   return (
     <div className="flex justify-between w-full h-screen">
       <LabDataTable data={data} link="verifikasi-data/" />
     </div>
-  )
+  );
 }
